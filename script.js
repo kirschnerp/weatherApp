60459173218d69fc16b3731560d54a7d7f666f42
Paul Kirschner <p.kirschner01@gmail.com>
2020-01-11 21:27:40 -0500

    should have committed earlier got the current conditions working onto the 5 day forecast

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 $(document).ready(function() {
-// This is our API key
+//date and time
+var dateTime = moment().format('MMMM Do YYYY');
+
+//input from text field
+$(".btn").on("click",function(event){
+  event.preventDefault();
+  var userinput = $("#city-name").val(); 
+ console.log(userinput);
+ // This is our API key
 var APIKey = "46e10682b797bc3fd80bbc8e8f437f42";
-
-  
 // QueryURL
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q= +textInput + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userinput + ",us&appid=" + APIKey;
 
-// Here we run our AJAX call to the OpenWeatherMap API
+ // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
   url: queryURL,
   method: "GET"
 })
+
   // We store all of the retrieved data inside of an object called "response"
   .then(function(response) {
 
-    // Log the queryURL
     console.log(queryURL);
-
-    // Log the resulting object
     console.log(response);
 
     // Transfer content to HTML
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".temp").text("Temperature (F) " + response.main.temp);
+    $(".city").html("<h1>" + response.name + " " + dateTime, "</h1>");
+    $(".temp").html("<h2>" + "Temperature (F) " + response.main.temp, "</h2>");
+    $(".humidity").html("<h2>" + "Humidity: " + response.main.humidity, "</h2>");
+    $(".wind").html("<h2>" + "Wind Speed: " + response.wind.speed, "</h2>");
+    $(".uv").html("<h2>" + "UV Index: " + response.wind.speed, "<h2>");
 
     // Converts the temp to Kelvin with the below formula
-    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-    $(".tempF").text("Temperature (Kelvin) " + tempF);
+   var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+   var tempFar = Math.round(tempF)
+    $(".temp").html("<h2>" + "Temperature (F): " + tempFar);
 
     // Log the data in the console as well
     console.log("Wind Speed: " + response.wind.speed);
     console.log("Humidity: " + response.main.humidity);
     console.log("Temperature (F): " + response.main.temp);
   });
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//5 day forecast
+
 
  
 
